fecc0df7770553f4374ce2289e439cecfac0a2ed
tarun dahiya <tarundh1997@gmail.com>
2022-10-07 16:33:49 +0530

    created new bmicalculor.html file and made changes to calculator.js to make bmi calculator work

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));   
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
@@ -12,15 +12,26 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-    console.log(req.body);
-
     var num1 = Number(req.body.num1)
     var num2 = Number(req.body.num2)
     var result = num1 + num2
 
-    res.send("The result of the calculator is " + result );
-});             
+    res.send("The result of the calculator is " + result);
+});
+
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var bmi = weight / (height * height);
+
+    res.send("Your BMI is  " + bmi );
+});
 
 app.listen(3000, function(){
     console.log("server is running on port 3000.");
-});             
+});
